7eeca677ba8172b766ca235794bb44abb805be4e
solida <solida@students.uni-marburg.de>
2023-03-06 17:29:45 +0100

    bug fixing: multilingo

--- _plugins/jekyll-multiple-languages-plugin/lib/jekyll-multiple-languages-plugin.rb
+++ _plugins/jekyll-multiple-languages-plugin/lib/jekyll-multiple-languages-plugin.rb
@@ -147,9 +147,17 @@ module Jekyll
       languages.each do |lang|
 
         # Language specific config/variables
-        @dest = "#{dest_org.join('/')}/#{lang}"
+      dest_org = "/Users/solidaneziri/IdeaProjects/visualproofs.github.io/_site".split("/")
+      @dest = "#{dest_org.join('/')}/#{lang}"
 
-        self.config['baseurl'] = baseurl_org + "/" + lang
+
+       if baseurl_org.empty?
+         @config['baseurl'] = "/#{lang}"
+       else
+         @config['baseurl'] = baseurl_org.join('/') + '/' + lang
+       end
+
+
         self.config['lang']    =                     lang
 
         puts "Building site for language: \"#{self.config['lang']}\" to: #{self.dest}"
